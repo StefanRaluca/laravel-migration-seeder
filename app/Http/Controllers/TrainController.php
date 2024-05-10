22838633cdd78a628d72a7f01059dedc437f6f6a
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-10 15:59:19 +0200

    add trains in the page use carbon library for today date

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
+use Illuminate\Support\Carbon;
+
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+    {
+        $now = Carbon::now();
+
+        $trains = Train::whereDate('departure_time', $now)->get();
+        return view('home', compact('trains'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,22 @@
 @extends('layouts.app')
 @section('content')
-    <h1>welcome</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <ul class="list-group">
+                    @foreach ($trains as $train)
+                        <li class="list-group-item">
+                            <h3>{{ $train->Train_Code }}</h3>
+                            <p> {{ $train->Company }}</p>
+                            <p>{{ $train->Departure_station }}</p>
+                            <p> {{ $train->Arrival_station }}</p>
+                            <p>{{ $train->Departure_time }}</p>
+                            <p>{{ $train->Arrival_time }}</p>
+                            <p>{{ $train->Carriages_number }}</p>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 @endsection
