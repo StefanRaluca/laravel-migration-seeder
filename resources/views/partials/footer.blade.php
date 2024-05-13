0f20e7e282c304df7113dddfd9d722c669c265c1
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-13 15:38:48 +0200

    add footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,40 @@
-<footer>footer</footer>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <h5>Company</h5>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5>Quick Links</h5>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms of Service</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5>Follow Us</h5>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#"> Twitter</a></li>
+                    <li><a href="#"> Instagram</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h5>Newsletter</h5>
+                <form>
+                    <input type="email" class="form-control" placeholder="Type your email">
+                    <button type="submit" class="btn btn-primary mt-1">Subscribe</button>
+                </form>
+            </div>
+        </div>
+        <div class="copyright text-center">
+            <p>❤️ 2024 By Boolean</p>
+        </div>
+    </div>
+</footer>
